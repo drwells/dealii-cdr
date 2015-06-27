5c2a49dc4c01f1d8c4980e412f7c672b0f0bfd3f
David Wells <drwells@vt.edu>
2015-06-27 12:52:23 -0400

    Split the system matrix file into three parts. Much like what deal.II does, I moved the actual implementation into a separate template file. The source file contains the instantiations I plan on using.

---
+++ common/system_matrix.cc
@@ -0,0 +1,42 @@
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+
+#include "parameters.h"
+#include "system_matrix.h"
+#include "system_matrix.templates.h"
+
+template
+void create_system_matrix<2, SparseMatrix<double>>
+(const DoFHandler<2>     &dof_handler,
+ const QGauss<2>         &quad,
+ const FunctionParser<2> &convection_function,
+ const ConstraintMatrix  &constraints,
+ const CDR::Parameters   &parameters,
+ SparseMatrix<double>    &system_matrix);
+
+template
+void create_system_matrix<3, SparseMatrix<double>>
+(const DoFHandler<3>     &dof_handler,
+ const QGauss<3>         &quad,
+ const FunctionParser<3> &convection_function,
+ const ConstraintMatrix  &constraints,
+ const CDR::Parameters   &parameters,
+ SparseMatrix<double>    &system_matrix);
+
+template
+void create_system_matrix<2, TrilinosWrappers::SparseMatrix>
+(const DoFHandler<2>            &dof_handler,
+ const QGauss<2>                &quad,
+ const FunctionParser<2>        &convection_function,
+ const ConstraintMatrix         &constraints,
+ const CDR::Parameters          &parameters,
+ TrilinosWrappers::SparseMatrix &system_matrix);
+
+template
+void create_system_matrix<3, TrilinosWrappers::SparseMatrix>
+(const DoFHandler<3>            &dof_handler,
+ const QGauss<3>                &quad,
+ const FunctionParser<3>        &convection_function,
+ const ConstraintMatrix         &constraints,
+ const CDR::Parameters          &parameters,
+ TrilinosWrappers::SparseMatrix &system_matrix);
